d4d726e5ad6a9b5f2e8a03bbf41342cc43d711fb
rampion19 <princegupta.pg19@gmail.com>
2023-01-26 19:30:14 +0530

    made two components seperatly and date and details

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,19 +1,12 @@
 import './Expenseitem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 function ExpenseItem(props) {
-
-    // const expenseDate = new Date(2023,0,26);
-    // const expenseTitle = 'Pizza';
-    // const expenseAmount = 300;
-    // const locationOfExpenditure = 'food';
-
-    return (<div className="expense-item">
-        <div>{props.date.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <h2>{props.catagory}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
-        </div>
-    </div>)
+    return (
+    <div className="expense-item">
+        <ExpenseDate date ={props.date} />
+        <ExpenseDetails title = {props.title}  catagory = {props.catagory} amount={props.amount}/>
+    </div>
+    )
 }
-
 export default ExpenseItem;
