d4e59b0910eae60959f378bcb4814de933301cc1
rampion19 <princegupta.pg19@gmail.com>
2023-01-26 18:07:47 +0530

    expenseItem file with four components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
 
+
+import ExpenseItem from './components/Expenseitem';
 function App() {
+  const expenses = [
+    {
+      title: 'Pizza',
+      amount: 300,
+      date: new Date(2023, 0, 26),
+      catagory: "food"
+    },
+    {
+      title: 'Camera',
+      amount: 15000,
+      date: new Date(2023, 0, 12),
+      catagory: "electronics"
+    },
+    {
+      title: 'Car',
+      amount: 700000,
+      date: new Date(2023, 0, 22),
+      catagory: "vehicle"
+    },
+    {
+      title: 'Books',
+      amount: 1500,
+      date: new Date(2023, 0, 25),
+      catagory: "education"
+    }
+  ]
+  
+  // for(let i=0; i<expenses.length; i++);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lets get started!</h2>
+      
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        catagory={expenses[0].catagory}>
+      </ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        catagory={expenses[1].catagory}>
+      </ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        catagory={expenses[2].catagory}>
+      </ExpenseItem>
+
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+        catagory={expenses[3].catagory}>
+      </ExpenseItem>
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,9 +1,19 @@
-function ExpenseItem() {
-    return (<div><h2>Expense item!</h2>
-        <h3>Food Rs 10</h3>
-        <h3>Petrol Rs 100</h3>
-        <h3>Movies Rs 200</h3>
-        </div>)
+import './Expenseitem.css';
+function ExpenseItem(props) {
+
+    // const expenseDate = new Date(2023,0,26);
+    // const expenseTitle = 'Pizza';
+    // const expenseAmount = 300;
+    // const locationOfExpenditure = 'food';
+
+    return (<div className="expense-item">
+        <div>{props.date.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <h2>{props.catagory}</h2>
+            <div className='expense-item__price'>{props.amount}</div>
+        </div>
+    </div>)
 }
 
 export default ExpenseItem;
